df53c429f9c49209e18c90d22f48f726bd758ec8
Sumon Chandra <80484412+sumon-chandra@users.noreply.github.com>
2025-08-10 00:39:02 +0600

    implement error handler functions

--- src/middlewares/global-error-handler.ts
+++ src/middlewares/global-error-handler.ts
@@ -2,11 +2,40 @@ import { NextFunction, Request, Response } from 'express';
 import httpStatusCode from 'http-status-codes';
 import { envVars } from '../config/env';
 import AppError from '../error-helpers/app-error';
+import mongoose from 'mongoose';
+
+const handlerDuplicateError = (err: any) => {
+    const matchedArray = err.message.match(/"([^"]*)"/)
+    return {
+        statusCode: httpStatusCode.FORBIDDEN,
+        message: `${matchedArray[1]} already exist!!`
+    }
+}
+
+const handlerCastError = (err: mongoose.Error) => {
+    console.log(err)
+    return {
+        statusCode: httpStatusCode.FORBIDDEN,
+        message: "Invalid MongoDB ObjectID. Please provide a valid id"
+    }
+}
 
 export const globalErrorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
 
     let statusCode = 500
     let message = "Something Went Wrong!!"
+
+    if (error.code === 11000) {
+        const simplifiedErr = handlerDuplicateError(error)
+        statusCode = simplifiedErr.statusCode
+        message = simplifiedErr.message
+    } else if (error.name === "CastError") {
+        const simplifiedErr = handlerCastError(error)
+        statusCode = simplifiedErr.statusCode
+        message = simplifiedErr.message
+    } else if (error.name === "ZodError") {
+
+    }
 
     if (error instanceof AppError) {
         statusCode = error.statusCode
